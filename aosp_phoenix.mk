db1a3115b87341c3591e8a8b760e35ad9bde93ff
YuvaRaj-BT <yuvarajv952@gmail.com>
2020-04-05 14:13:28 +0530

    Aosp Bringup

--- aosp_phoenix.mk
+++ aosp_phoenix.mk
@@ -10,18 +10,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common BlissRoms stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
-
-# Inherit GApps
-$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+# Inherit some common Rom stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bliss_phoenix
+PRODUCT_NAME := aosp_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
+TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
